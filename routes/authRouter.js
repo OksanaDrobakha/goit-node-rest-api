72cb55d6a4dda69f17c80768f91e5f49012580f6
OksanaDrobakha <6249784@gmail.com>
2024-03-24 21:35:48 +0400

    registration and login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,20 @@
 import express from "express";
+import authController from "../controllers/authController.js";
+import validateBody from "../helpers/validateBody.js";
+import { userSignupSchema, userSigninSchema } from "../schemas/userSchemas.js";
 
 const authRouter = express.Router();
+
+authRouter.post(
+  "/signup",
+  validateBody(userSignupSchema),
+  authController.signup
+);
+
+authRouter.post(
+  "/signin",
+  validateBody(userSigninSchema),
+  authController.signin
+);
 
 export default authRouter;
